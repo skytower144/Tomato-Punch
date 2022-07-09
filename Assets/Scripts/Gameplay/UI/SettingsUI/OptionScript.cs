04305e0d546826d99922991458e6ccea2b9cd1c0
skytower144 <skytower144@daum.net>
2022-07-10 01:46:55 +0900

    Option Menu Implementation
    * Created & changed option menu sprites.
    
    * Implemented option scroll function.

--- Assets/Scripts/Gameplay/UI/SettingsUI/OptionScript.cs
+++ Assets/Scripts/Gameplay/UI/SettingsUI/OptionScript.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using DG.Tweening;
 
 public class OptionScript : MonoBehaviour
 {
     [SerializeField] private PlayerMovement playerMovement;
-    [SerializeField] private List <GameObject> optionList;
+    [SerializeField] private GameObject optionBase;
+    [SerializeField] private List <TextMeshProUGUI> optionTexts;
     [System.NonSerialized] public bool is_busy_option;
     private int optionNumber;
     void Update()
@@ -22,6 +24,14 @@ public class OptionScript : MonoBehaviour
             {
                 CloseOptions();
             }
+            else if(Input.GetKeyDown(KeyCode.Q))
+            {
+                SwitchOption("-");
+            }
+            else if(Input.GetKeyDown(KeyCode.E))
+            {
+                SwitchOption("+");
+            }
         }
     }
     public void OpenOptions()
@@ -29,19 +39,65 @@ public class OptionScript : MonoBehaviour
         is_busy_option = true;
 
         optionNumber = 0;
-        optionList[0].SetActive(true);
-
-        DOTween.Rewind("open_option");
-        DOTween.Play("open_option");
+        optionBase.SetActive(true);
     }
     private void CloseOptions()
     {
         is_busy_option = false;
-        DOTween.Rewind("close_option");
-        DOTween.Play("close_option");
+        Invoke("TurnoffOption", 0.1f);
     }
     public void TurnoffOption()
     {
-        optionList[optionNumber].SetActive(false);
+        optionBase.SetActive(false);
+    }
+    private void SwitchOption(string direction)
+    {
+        
+        if (direction == "+")
+        {
+            if (optionNumber == 0)
+            {
+                DOTween.Rewind("option_0_1");
+                DOTween.Play("option_0_1");
+            }
+            else if (optionNumber == 1)
+            {
+                DOTween.Rewind("option_1_2");
+                DOTween.Play("option_1_2");
+            }
+            if (optionNumber < 2)
+                UncolorText();
+            optionNumber += 1;
+            optionNumber = Mathf.Clamp(optionNumber, 0, 2);
+        }
+        else if (direction == "-")
+        {
+            if (optionNumber == 2)
+            {
+                DOTween.Rewind("option_2_1");
+                DOTween.Play("option_2_1");
+            }
+            else if (optionNumber == 1)
+            {
+                DOTween.Rewind("option_1_0");
+                DOTween.Play("option_1_0");
+            }
+            if (optionNumber > 0)
+                UncolorText();
+            optionNumber -= 1;
+            optionNumber = Mathf.Clamp(optionNumber, 0, 2);
+        }
+    }
+    private void UncolorText()
+    {
+        optionTexts[optionNumber].color = new Color32(112, 82, 75, 255);
+    }
+    private void ColorText()
+    {
+        optionTexts[optionNumber].color = new Color32(97, 125, 97, 255);
+    }
+    public void DisplayOption()
+    {
+        ColorText();
     }
 }
