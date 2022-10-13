67430e7100b4ecf3b41da4595f4a32fb261560ad
skytower144 <skytower144@daum.net>
2022-10-13 16:14:36 +0900

    Implementing QuickSave - 2
    * Created quick save zone prefab to add on scenes.
    
    - Added a particle effect.
    - Added a save icon popup effect via DoTween.

--- Assets/Scripts/Saving/QuickSave.cs
+++ Assets/Scripts/Saving/QuickSave.cs
@@ -1,17 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class QuickSave : MonoBehaviour
 {
-    [SerializeField] private bool isQuickSaved = false;
+    [SerializeField] ParticleSystem sparkleEffect;
+    private bool isQuickSaved = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!isQuickSaved && (collision.tag == "Player"))
         {
             isQuickSaved = true;
-            Debug.Log("Proceeding quick save...");
+            SparkleEffect();
+            PopupEffect();
             GameManager.gm_instance.save_load_menu.ProceedSave(3);
         }
+    }
+
+    private void SparkleEffect()
+    {
+        var sparkle_em = sparkleEffect.emission;
+        sparkle_em.enabled = true;
+        sparkleEffect.Play();
+    }
+
+    private void PopupEffect()
+    {
+        DOTween.Rewind("savecat_popup");
+        DOTween.Play("savecat_popup");
     }
 }
