cc94021e8fc27bc1e7f79ad4cecf7e95037accc4
skytower144 <skytower144@daum.net>
2023-07-31 01:39:07 +0900

    Cat Follow Movement - 3
    * Fixed some sprite flicker issues
    
    * Added delay to PlayerStop recognition
    
    * Created/Added Cat walk animations and blend tree

---
+++ Assets/Scripts/Gameplay/NPC/NPCFollow.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFollow : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private int followDelay;
+    [SerializeField] private bool enableMove;
+
+    private float followSpeed = 8.9f;
+    private float stopTime;
+    private bool playerStopped;
+    private Transform leader;
+    private Queue<Vector2> record = new Queue<Vector2>();
+    private Rigidbody2D rb;
+
+    Vector2 direction;
+    float distance;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        leader = GameManager.gm_instance.player_movement.transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (!enableMove) return;
+        Watch();
+    }
+
+    private void Watch()
+    {
+        if (PlayerHasStopped()) {
+            if (!playerStopped) {
+                playerStopped = true;
+                stopTime = Time.time;
+            }
+
+            if (Time.time - stopTime > 0.1f) {
+                Animate(false);
+                return;
+            }
+        }
+        else
+            playerStopped = false;
+        
+        if (!record.Contains(leader.position))
+            record.Enqueue(leader.position);
+
+        if (record.Count > followDelay) {
+            Follow(record.Dequeue());
+        }
+    }
+
+    private void Follow(Vector2 movePos)
+    {
+        direction = movePos - rb.position;
+        distance = direction.magnitude;
+
+        if (distance < 0.01f) {
+            rb.position = movePos;
+            return;
+        }
+        else
+        {
+            // Calculate the movement speed based on followSpeed and Time.deltaTime
+            float movementSpeed = Mathf.Min(followSpeed * Time.deltaTime, distance);
+
+            // Move the cat towards the target position using the calculated speed
+            Vector2 amount = direction.normalized * movementSpeed;
+            rb.position += amount;
+        }
+        Animate(true, direction);
+    }
+
+    private void Animate(bool isAnimating, Vector2 direction = default)
+    {
+        if (anim.GetBool("isWalking") && !isAnimating)
+            FlattenPos();
+        
+        anim.SetBool("isWalking", isAnimating);
+
+        if (!isAnimating) {
+            anim.SetFloat("moveX", 0f);
+            anim.SetFloat("moveY", 0f);
+            return;
+        }
+        direction = direction.normalized;
+        anim.SetFloat("moveX", Mathf.Round(direction.x));
+        anim.SetFloat("moveY", Mathf.Round(direction.y));
+    }
+
+    private void FlattenPos()
+    {
+        transform.position = new Vector2(transform.position.x, Mathf.Floor(transform.position.y * 100f) / 100f);
+    }
+
+    private bool PlayerHasStopped()
+    {
+        return !GameManager.gm_instance.player_movement.playerAnim.GetBool("isWalking");
+    }
+}
